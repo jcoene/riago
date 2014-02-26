a466e74065e58d4c6e6861f98f315e1f7eaf8db9
Jason Coene <jcoene@gmail.com>
2014-02-26 09:37:42 -0600

    Improve test coverage, add tests for list keys and map reduce.

--- client_test.go
+++ client_test.go
@@ -1,20 +1,59 @@
 package riago
 
 import (
+	"encoding/json"
+	"fmt"
+	"regexp"
 	"testing"
 	"time"
 
 	. "github.com/smartystreets/goconvey/convey"
 )
 
+func inst(p *Profile) {
+	p.String()
+}
+
+func mapRedUnionQuery(bucket string, keys []string) (query string) {
+	query = `{"inputs":`
+
+	if len(keys) == 0 {
+		query += `"` + bucket + `"`
+	} else {
+		query += `[`
+		for i, key := range keys {
+			if i > 0 {
+				query += `,`
+			}
+			query += `["` + bucket + `", "` + key + `"]`
+		}
+		query += `]`
+	}
+
+	query += `,"query": [{"map":{"language":"erlang","module":"riak_kv_mapreduce","function":"map_object_value"}},{"reduce":{"language":"erlang","module":"riak_kv_mapreduce","function":"reduce_set_union"}}]}`
+
+	return
+}
+
 func TestClientInstance(t *testing.T) {
 	Convey("Client instance", t, func() {
 		client := NewClient("127.0.0.1:8087", 3)
 		dur := 500 * time.Millisecond
 
+		Convey("Can set an instrumenter", func() {
+			client.SetInstrumenter(inst)
+			So(client.instrumenter, ShouldEqual, inst)
+		})
+
 		Convey("Can set retry attempts", func() {
 			client.SetRetryAttempts(3)
 			So(client.retryAttempts, ShouldEqual, 3)
+		})
+
+		Convey("Can set retry delay", func() {
+			dur := time.Duration(500 * time.Millisecond)
+			client.SetRetryDelay(dur)
+			So(client.retryDelay, ShouldEqual, dur)
 		})
 
 		Convey("Can set read timeout", func() {
@@ -37,6 +76,24 @@ func TestClientInstance(t *testing.T) {
 func TestClientKV(t *testing.T) {
 	Convey("Client KV", t, func() {
 		client := NewClient("127.0.0.1:8087", 3)
+		client.SetInstrumenter(inst)
+		client.SetReadTimeout(2 * time.Second)
+		client.SetWriteTimeout(2 * time.Second)
+
+		Convey("Returns client errors properly", func() {
+			req := &RpbPutReq{}
+			_, err := client.Put(req)
+			So(err.Error(), ShouldContainSubstring, "required field")
+		})
+
+		Convey("Returns service errors properly", func() {
+			req := &RpbMapRedReq{
+				Request:     []byte("this isn't going to work at all"),
+				ContentType: []byte("application/json"),
+			}
+			_, err := client.MapRed(req)
+			So(err.Error(), ShouldContainSubstring, "invalid_json")
+		})
 
 		Convey("Can set and get bucket properties", func() {
 			two := uint32(2)
@@ -127,6 +184,91 @@ func TestClientKV(t *testing.T) {
 				}
 			}
 			So(found, ShouldEqual, true)
+		})
+
+		Convey("Can list keys", func() {
+			putValue := "{\"hello\": \"world\"}"
+			putContentType := "application/json"
+
+			n := 11
+			for i := 0; i < n; i++ {
+				putReq := &RpbPutReq{
+					Bucket: []byte("riago_test"),
+					Key:    []byte(fmt.Sprintf("client_test_list_keys_%d", i)),
+					Content: &RpbContent{
+						Value:       []byte(putValue),
+						ContentType: []byte(putContentType),
+					},
+				}
+
+				_, err := client.Put(putReq)
+				So(err, ShouldEqual, nil)
+			}
+
+			listReq := &RpbListKeysReq{
+				Bucket: []byte("riago_test"),
+			}
+			listResps, listErr := client.ListKeys(listReq)
+			So(listErr, ShouldEqual, nil)
+
+			found := 0
+			for _, r := range listResps {
+				for _, k := range r.GetKeys() {
+					if f, _ := regexp.Match("client_test_list_keys_", k); f {
+						found += 1
+					}
+				}
+			}
+
+			So(found, ShouldEqual, n)
+		})
+
+		Convey("Can map reduce", func() {
+			n := 8
+			keys := make([]string, n)
+
+			for i := 0; i < n; i++ {
+				k := fmt.Sprintf("client_test_mapred_%d", i)
+				v := fmt.Sprintf("{\"id\": %d}", i)
+
+				keys[i] = k
+				putReq := &RpbPutReq{
+					Bucket: []byte("riago_test"),
+					Key:    []byte(k),
+					Content: &RpbContent{
+						Value:       []byte(v),
+						ContentType: []byte("application/json"),
+					},
+				}
+
+				_, err := client.Put(putReq)
+				So(err, ShouldEqual, nil)
+			}
+
+			query := mapRedUnionQuery("riago_test", keys)
+
+			mapRedReq := &RpbMapRedReq{
+				Request:     []byte(query),
+				ContentType: []byte("application/json"),
+			}
+			mapRedResps, mapRedErr := client.MapRed(mapRedReq)
+			So(mapRedErr, ShouldEqual, nil)
+
+			found := 0
+			for _, r := range mapRedResps {
+				if r.Response == nil {
+					continue
+				}
+
+				var rvals []string
+				err := json.Unmarshal(r.GetResponse(), &rvals)
+				So(err, ShouldEqual, nil)
+				for _, _ = range rvals {
+					found += 1
+				}
+			}
+
+			So(found, ShouldEqual, n)
 		})
 	})
 }
